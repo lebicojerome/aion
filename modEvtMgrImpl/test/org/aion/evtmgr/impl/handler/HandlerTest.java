eec2745ecb002883a606bd163f10af1d9928b98c
William Zhai <you@example.com>
2018-07-24 09:43:56 -0400

    created unit tests for Events

---
+++ modEvtMgrImpl/test/org/aion/evtmgr/impl/handler/HandlerTest.java
@@ -0,0 +1,16 @@
+package org.aion.evtmgr.impl.handler;
+
+
+import org.aion.evtmgr.IHandler;
+import org.aion.evtmgr.impl.abs.AbstractHandler;
+
+public class HandlerTest {
+
+
+
+    public void blockHandlerTest(){
+        IHandler blkHdr = new BlockHandler();
+
+    }
+
+}
